cd9207b319fc124a141af013ed24d5fe6b143d7b
Coldsnipe_7902 <145302711+Colsnipe7902@users.noreply.github.com>
2024-03-03 11:04:10 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,4 @@
+option = int(input ("What is the first number you want to add? "))
+option2 = int(input ("What is the second number you want to add? "))
+answer = option + option2
+print(f"Your total answer is {option} + {option2} = {answer}")
